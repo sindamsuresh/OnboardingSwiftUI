2d0202f6c79693ad3426b2673892f78236d3ac1e
SureshSindam <sindam.suresh@gmail.com>
2024-01-27 15:50:19 -0600

    Initial Commit

---
+++ Onboarding In SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Onboarding In SwiftUI
+//
+//  Created by Suresh Sindam on 1/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
